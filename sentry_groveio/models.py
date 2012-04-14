b8863829cfeea1237811d2111a86d2b92dc97980
George Hickman <george@ghickman.co.uk>
2012-04-14 21:38:53 +0100

    Let the user decide what to display in the first section of the message
    This is done mainly to provide nicer output from heroku hosted
    applications where the server name is a random string and usually not
    meaningful.

--- sentry_groveio/models.py
+++ sentry_groveio/models.py
@@ -19,9 +19,15 @@ from django.utils import simplejson as json
 logger = logging.getLogger('sentry.plugins.groveio')
 
 
+form_options = (
+    ('project.name', 'Project Name'),
+    ('server_name', 'Server Name')
+)
+
 class GroveIoOptionsForm(forms.Form):
     token = forms.CharField(help_text="Your Grove.io channel token.")
     service_name = forms.CharField(help_text="Name of the service (displayed in the message)", initial='Sentry')
+    message_identifier = forms.ChoiceField(choices=form_options, help_text='')
     url = forms.CharField(help_text="(Optional) Service URL for the web client", required=False)
     icon_url = forms.CharField(help_text="(Optional) Icon for the service", required=False)
 
@@ -40,13 +46,30 @@ class GroveIoMessage(Plugin):
     def is_configured(self, project):
         return all((self.get_option(k, project) for k in ('token', 'service_name')))
 
+    def get_message_identifier(self, event):
+        """
+        Take the option selected in the management form and turn it into an
+        object attribute to get the string we need.
+        """
+        method = self.get_option('message_identifier', event.project)
+        if not method:
+            # in case this option hasn't been set before
+            return getattr(event, 'server_name')
+        instance = event
+        for attr in method.split('.'):
+            instance = getattr(instance, attr)
+        return instance
+
     def post_process(self, group, event, is_new, is_sample, **kwargs):
         if not is_new:
             return
         token = self.get_option('token', event.project)
         service = self.get_option('service_name', event.project)
         if token and service:
-            message = '[%s] %s: %s' % (event.server_name, event.get_level_display().upper(), event.error().encode('utf-8').split('\n')[0])
+            message = '[%s] %s: %s' % (
+                self.get_message_identifier(event),
+                event.get_level_display().upper(),
+                event.error().encode('utf-8').split('\n')[0])
             self.send_payload(token, service, event, group, message)
 
     def send_payload(self, token, service, event, group, message):
